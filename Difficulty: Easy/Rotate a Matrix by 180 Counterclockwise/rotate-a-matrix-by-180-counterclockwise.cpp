371b570a2d6273d5e8b8a999873c0c758747f431
Munaf <munafmd9849@gmail.com>
2025-05-18 15:44:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate a Matrix by 180 Counterclockwise/rotate-a-matrix-by-180-counterclockwise.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    void rotateMatrix(vector<vector<int>>& mat) {
+        reverse(mat.begin(),mat.end());
+        int n=mat.size();
+        for(int i=0;i<n;i++){
+            reverse(mat[i].begin(),mat[i].end());
+        }
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution ob;
+        ob.rotateMatrix(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                cout << matrix[i][j] << " ";
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
