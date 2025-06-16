32d9c799b172662b50b21a08b807cb9a363bd64b
Munaf <munafmd9849@gmail.com>
2025-06-16 19:48:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        // Your code here
+        unordered_map<int ,int> mapp;
+        for(auto i:a){
+            mapp[i]=0;
+        }
+        for(auto i:a){
+            mapp[i]++;
+        }
+        
+        for(auto i:b){
+            if(mapp.find(i)!=mapp.end()){
+                if(mapp[i]<=0){
+                    return false;
+                }
+                else
+                    mapp[i]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
