9eeb88bbdc21c9df24fbc6a1d03940cb23d2c24b
Munaf <munafmd9849@gmail.com>
2025-06-24 00:13:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Row Sum in a Matrix/row-sum-in-a-matrix.cpp
+++ Difficulty: Easy/Row Sum in a Matrix/row-sum-in-a-matrix.cpp
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function Template for C++
 class Solution {
   public:
     vector<int> rowSum(vector<vector<int>>& mat) {
@@ -21,28 +12,3 @@ class Solution {
         return ans;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc-- > 0) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> mat(n, vector<int>(m, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++)
-                cin >> mat[i][j];
-        }
-        Solution sln;
-        vector<int> ans = sln.rowSum(mat);
-        for (int i = 0; i < n; i++)
-            cout << ans[i] << " ";
-        cout << endl << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
