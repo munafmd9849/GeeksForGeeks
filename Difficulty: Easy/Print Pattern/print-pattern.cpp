d5989cf06dff2f0b93d59f86d39c8eb9e01e893a
Munaf <munafmd9849@gmail.com>
2025-06-28 23:37:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Print Pattern/print-pattern.cpp
+++ Difficulty: Easy/Print Pattern/print-pattern.cpp
@@ -2,22 +2,29 @@
 
 class Solution {
   public:
-  
-    vector<int> pattern(int N) {
+    vector<int> pattern(int n) {
         // code here
-        vector<int> ans;
-        pat(N,ans);
-        return ans;
-        
+        if(n<=0){
+            return {n};
+        }
+        vector<int> a;
+        a.push_back(n);
+        pat(a,n-5);
+        return a;
     }
-    void pat(int n,vector<int> &a){
+    int it=-5;
+    void pat(vector<int> &a,int n){
+        if(n==a[0]){
+            a.push_back(a[0]);
+            return;
+        }
         
         if(n<=0){
-            a.push_back(n);
-            return ;
+            it=5;
         }
         a.push_back(n);
-        pat(n-5,a);
-        a.push_back(n);
+        pat(a,n+it);
+        
+        
     }
 };
