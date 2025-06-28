43b46608755caa021ad87b2364c7016e47dd19c6
Munaf <munafmd9849@gmail.com>
2025-06-29 02:25:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum of GCDs/sum-of-gcds.cpp
+++ Difficulty: Easy/Sum of GCDs/sum-of-gcds.cpp
@@ -1,37 +1,32 @@
 class Solution {
-public:
-    int eulerTotient(int n) {
-    int result = n;
-    for (int p = 2; p * p <= n; ++p) {
-        if (n % p == 0) {
-            while (n % p == 0)
-                n /= p;
-            result -= result / p;
-        }
-    }
-    if (n > 1)
-        result -= result / n;
-    return result;
-}
-
-int sumOfGCDofPairs(int N) {
-    int sum = 0;
-    for (int i = 1; i * i <= N; ++i) {
-        if (N % i == 0) {
-            int d1 = i;
-            int d2 = N / i;
-            
-
-            int count1 = eulerTotient(N / d1);
-            sum += d1 * count1;
-            
-       
-            if (d1 != d2) {
-                int count2 = eulerTotient(N / d2);
-                sum += d2 * count2;
+  public:
+    int phi(int n) {
+        int result = n;
+        for (int p = 2; p * p <= n; ++p) {
+            if (n % p == 0) {
+                while (n % p == 0)
+                    n /= p;
+                result -= result / p;
             }
         }
+        if (n > 1)
+            result -= result / n;
+        return result;
     }
-    return sum;
-}
+
+    long long int sumOfGCDofPairs(int N) {
+        long long sum = 0;
+        for (int d = 1; d * d <= N; d++) {
+            if (N % d == 0) {
+                int div1 = d;
+                int div2 = N / d;
+
+                sum += 1LL * div1 * phi(N / div1);
+                if (div1 != div2) {
+                    sum += 1LL * div2 * phi(N / div2);
+                }
+            }
+        }
+        return sum;
+    }
 };
