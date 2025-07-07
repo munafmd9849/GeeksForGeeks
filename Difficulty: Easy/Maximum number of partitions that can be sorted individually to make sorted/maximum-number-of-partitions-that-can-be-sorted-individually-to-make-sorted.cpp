108aa5fef886eafbd950bff56993eca54d0024a9
Munaf <munafmd9849@gmail.com>
2025-07-07 19:32:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Maximum number of partitions that can be sorted individually to make sorted/maximum-number-of-partitions-that-can-be-sorted-individually-to-make-sorted.cpp
+++ Difficulty: Easy/Maximum number of partitions that can be sorted individually to make sorted/maximum-number-of-partitions-that-can-be-sorted-individually-to-make-sorted.cpp
@@ -4,13 +4,11 @@ class Solution {
         // code here
         int n = arr.size();
         int maxi=0;
-        int c=0;
+        int c=0,index=0;
         for(int i=0;i<n;i++){
-            if(arr[i]>maxi)
-            maxi=arr[i];
-            
-            if(maxi==i)
-            c++;
+            index+=i;
+            maxi+=arr[i];
+            if(maxi==index)c++;
         }
         return c;
     }
