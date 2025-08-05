a753ac4a5a87d3a60fc1a572f9f7583eff318073
Munaf <munafmd9849@gmail.com>
2025-08-05 23:39:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -1,35 +1,18 @@
-// User function template for C++
 class Solution {
   public:
     int rowWithMax1s(vector<vector<int>> &arr) {
         int p2=arr[0].size()-1;
+        int p1=0;
         int ans=-1;
-        int maxi=0;
-        for(int i=0;i<arr.size();i++){
-            int count1=0;
-            for(int j=arr[0].size()-1;j>=0;j--){
-                if(arr[i][j]==1){
-                    count1++;
-                }
-                else{
-                    break;
-                }
+        while(p2>=0 && p1<arr.size()){
+            if(arr[p1][p2]==1){
+                p2--;
+                ans=p1;
             }
-            if (count1>maxi){
-                ans=i;
-                maxi=count1;
+            else{
+                p1++;
             }
         }
-        
         return ans;
     }
 };
-
-
-
-
-
-    // 0 1 1 1
-    // 0 0 1 1
-    // 1 1 1 1
-    // 0 0 0 0 
