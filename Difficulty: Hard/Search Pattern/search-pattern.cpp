1f04111b4455f9a0e1d4f7db9d1f275cf7f58aae
Munaf <munafmd9849@gmail.com>
2025-08-25 18:44:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Search Pattern/search-pattern.cpp
+++ Difficulty: Hard/Search Pattern/search-pattern.cpp
@@ -1,44 +1,41 @@
 class Solution {
   public:
-    vector<int> lps(string s){
-            int n=s.length();
-            vector<int> ans(n);
-            ans[0]=0;
-            for(int i=1;i<n;i++){
-                int x=ans[i-1];
-                while(s[i]!=s[x]){
-                    if(x==0){
-                        x=-1;
-                        break;
-                    }
-                    
-                    x=ans[x-1];
-                }
-                ans[i]=x+1;
-            }
-            return ans;
-    }
-    
     vector<int> search(string &pat, string &txt) {
         // code here
-        string s=pat+"#"+txt;
-        
-        vector<int> lpsarr=lps(s);
-        
-        int p=pat.length();
-        
-        vector<int> c;
-        
-        for(int i=p+1;i<lpsarr.size();i++){
-            if(lpsarr[i]==p){
-                c.push_back(i-2*p);
+        int n = txt.length(), x =0;
+        vector<int> res;
+        vector<int>lps(pat.length()+1,0);
+        pat+='@';
+ 
+        lps[0] = 0;
+        for(int i=1; i<pat.length(); ++i){
+            int x = lps[i-1];
+            while(pat[x] != pat[i]){
+                if(x == 0){
+                    x = -1;
+                    break;
+                }
+                x = lps[x-1];
             }
+ 
+            lps[i] = x + 1;
         }
-        // for(int i:lpsarr){
-        //     cout<<i;
-        // }
-        
-        return c;
-        
+ 
+ 
+ 
+        for(int i=0; i<n; ++i){
+            while(txt[i] != pat[x]){
+                if(x==0) {
+                    x = -1; 
+                    break;
+                }
+                x = lps[x-1];
+            }
+ 
+            x+=1;
+            if(x == pat.length()-1) res.push_back(i - pat.length() + 2);
+        }
+ 
+        return res;
     }
 };
