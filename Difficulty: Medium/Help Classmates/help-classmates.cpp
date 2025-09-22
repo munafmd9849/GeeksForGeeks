6b8ea21a3b00e0ef4f6ec04f4ee4b3629ea34a4f
Munaf <munafmd9849@gmail.com>
2025-09-22 22:54:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Help Classmates/help-classmates.cpp
@@ -0,0 +1,23 @@
+// User function Template for C++
+
+class Solution {
+
+  public:
+    vector<int> help_classmate(vector<int> arr, int n) {
+        // Your code goes here
+        stack<int> s;
+        
+        vector<int> ans(arr.size(),-1);
+        
+        for(int i=arr.size()-1;i>=0;i--){
+            while(s.size()>0 && arr[i]<=s.top()){
+                s.pop();
+            }
+            if(s.size()>0){
+                ans[i]=s.top();
+            }
+            s.push(arr[i]);
+        }
+        return ans;
+    }
+};
