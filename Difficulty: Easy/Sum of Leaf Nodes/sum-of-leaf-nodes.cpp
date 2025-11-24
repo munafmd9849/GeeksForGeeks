a4d40498a02ff0dc56995117da19ee5fff3c77dd
Munaf <191462807+munafmd9849@users.noreply.github.com>
2025-11-24 22:51:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of Leaf Nodes/sum-of-leaf-nodes.cpp
@@ -0,0 +1,28 @@
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+// Function to return a list containing the level order traversal in spiral form.
+class Solution {
+  public:
+    int leafSum(Node* root) {
+        // code here
+        if(root==nullptr){
+            return 0;
+        }
+        if(root->left==nullptr && root->right==nullptr){
+            return root->data;
+        }
+        return leafSum(root->left)+leafSum(root->right);
+    }
+};
